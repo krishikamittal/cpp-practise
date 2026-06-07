23ac6868e0fb11e7378276893e3c6db772f993ab
Krishika Mittal <krishikamittal15@gmail.com>
2026-06-07 13:43:16 +0530

    Create basic_constructor.cpp

---
+++ basic_constructor.cpp
@@ -0,0 +1,32 @@
+// Program: Constructor-Destructor using Inheritance
+// Topic: Object-Oriented Programming - Inheritance
+#include <iostream>
+using namespace std;
+
+class A {
+public:
+    A() { cout << "A Constructor\n"; }
+    ~A() { cout << "A Destructor\n"; }
+};
+
+class B : virtual public A {
+public:
+    B() { cout << "B Constructor\n"; }
+    ~B() { cout << "B Destructor\n"; }
+};
+
+class C : virtual public A {
+public:
+    C() { cout << "C Constructor\n"; }
+    ~C() { cout << "C Destructor\n"; }
+};
+
+class D : public B, public C {
+public:
+    D() { cout << "D Constructor\n"; }
+    ~D() { cout << "D Destructor\n"; }
+};
+
+int main() {
+    D obj;
+}
